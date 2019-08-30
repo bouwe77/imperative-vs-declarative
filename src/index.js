bafbf6ba3125d273617705f5972e25f3498979d1
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-08-30 11:27:06 +0200

    .

--- src/index.js
+++ src/index.js
@@ -1,5 +1,26 @@
 import $ from "jquery";
+var serializeForm = require("form-serialize");
+
+var todos = ["Afwassen", "Stofzuigen", "Was opvouwen"];
 
 var $app = $("#app")
-  .append("<h1>Hello world</h1>")
+  .append("<h1>My To Do App</h1>")
+  .append("<ul>")
   .appendTo(document.body);
+
+displayTodos();
+
+function displayTodos() {
+  $("ul").html("");
+  todos.forEach(todo => {
+    $("ul").append(`<li>${todo}</li>`);
+  });
+}
+
+$app.append(`<form><input name="newTodo"/></form>`).on("submit", event => {
+  event.preventDefault();
+  const values = serializeForm(event.target, { hash: true });
+  todos.push(values.newTodo);
+  event.target.reset();
+  displayTodos();
+});
